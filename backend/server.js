c09948eb7218b25e4438fd99ffeb205da6fa1baa
hosseintalebi <htalebi@hotmail.com>
2018-05-02 20:11:47 -0600

    frontend / backend distinction

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const express = require('express')
+const socketIO = require('socket.io')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
+
+io.on('connection', (socket) => {
+  console.log('new user connected')
+})
+
+server.listen(PORT, () => console.log(`app is listening on port ${PORT}`))
